dacc3de510e5e54d6e1a35ae1708e5f054d56511
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-07-28 19:31:23 -0400

    Completed the landing page styles

--- src/Components/UI/Navbar/Navbar.js
+++ src/Components/UI/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
             <div className="Navbar-container">
                 <div className="Navbar-site-title">
                     <img alt="logo" className="Navbar-site-title__logo" src={require('../../../Assets/font.svg')} />
-                    <h2 className="Navbar-site-title__name">Article</h2>
+                    <h2 className="heading-secondary--main">Article</h2>
                 </div>
 
                 <nav>
